ee6cf101ba8c133c851067d5a8febe6967c4de00
Czakero <bartosz.pyrz96@gmail.com>
2018-10-23 15:11:46 +0200

    Implement two constructors for dynamic array initialization

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,5 +2,25 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_SIZE = 0;
+    private int capacity;
+    private int size;
+    private int[] data;
 
+    public DynamicIntArray() {
+        this.data = new int[DEFAULT_CAPACITY];
+        this.capacity = DEFAULT_CAPACITY;
+        this.size = DEFAULT_SIZE;
+    }
+
+    public DynamicIntArray(int capacity) {
+        this.data = new int[capacity];
+        this.capacity = capacity;
+        this.size = DEFAULT_SIZE;
+    }
+
+    public void add(int number) {
+
+    }
 }
