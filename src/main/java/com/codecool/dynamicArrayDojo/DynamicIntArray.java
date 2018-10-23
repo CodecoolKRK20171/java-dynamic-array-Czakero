6a425619c946ad200b3e055f84ba8de0389fcf90
Czakero <bartosz.pyrz96@gmail.com>
2018-10-23 15:47:56 +0200

    Implement add method with over-capacity handling and override toString method

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,5 +1,7 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.Arrays;
+
 // put your code here!
 public class DynamicIntArray {
     private static final int DEFAULT_CAPACITY = 10;
@@ -21,6 +23,20 @@ public class DynamicIntArray {
     }
 
     public void add(int number) {
+        if (this.size + 1 == this.capacity) {
+            this.capacity += DEFAULT_CAPACITY;
+            this.data = Arrays.copyOf(this.data, this.capacity);
+        }
+        this.data[this.size] = number;
+        this.size += 1;
+    }
 
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(" ");
+            sb.append(this.data[i]);
+        }
+        return sb.toString();
     }
 }
